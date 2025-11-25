4c9e8adea340bae44ebcf4df86085c6c898c0891
nikhilchauhan0105-source <nikhilchauhan0105@gmail.com>
2025-11-25 17:32:14 +0530

    selection quantities display scrollbar added

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -33,21 +33,26 @@ const AddItems = () => {
         <Plus size={15} /> Add custom items
       </button>
 
-        <div className="w-full shadow-sm/30 rounded-xl mt-4 py-3 px-2 ">
-      {Object.keys(quantities).map((category) => (
-          <div className="max-w-[500px] rounded-xl p-4 m-auto shadow-sm/30 my-2" key={category}>
+      <div className="w-full shadow-sm/30 rounded-xl max-h-[250px] custom-scrollbar overflow-y-scroll  bg-blue-300/10 mt-4 py-2 px-2 ">
+        {Object.keys(quantities).map((category) => (
+          <div
+            className="max-w-[510px] rounded-xl p-4 m-auto bg-white shadow-sm/30 my-2"
+            key={category}
+          >
             {Object.keys(quantities[category]).map((item) => (
               <div className="flex my-1 items-center justify-between pr-10">
-                <p className="text-xs" key={item}>
+                <p className="text-sm" key={item}>
                   {item}
                 </p>
-                <span className="text-xs px-2 py-0.5 shadow-amber-600  shadow-xs/40  rounded-lg">{quantities[category][item]}</span>
+                <span className="text-xs px-2 py-0.5 shadow-amber-600  shadow-xs/40  rounded-lg">
+                  {quantities[category][item]}
+                </span>
               </div>
             ))}
             <h3 className="text-xs text-textlight">{category}</h3>
           </div>
-      ))}
-        </div>
+        ))}
+      </div>
     </div>
   );
 };
