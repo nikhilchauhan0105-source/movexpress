7d746c7cca1df130937f44ec28cdf022610fb1d7
nikhilchauhan0105-source <nikhilchauhan0105@gmail.com>
2025-12-05 11:46:44 +0530

    get data from database

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -28,21 +28,31 @@ const ShowData = () => {
   }, []);
 
   // safe first document
-  const oneData = data?.[1] ?? null; // <- this prevents "can't read" errors
 
   return (
     <>
       {loading ? (
         <p>Loading...</p>
-      ) : !oneData ? (
+      ) : !data ? (
         <p>No data found</p>
       ) : (
-        <div className="bg-gray-500 rounded-2xl p-6 mt-40 max-w-[600px] w-full m-auto  flex flex-col gap-5 text-white text-center text-2xl">
-          <p>Loading time : {oneData.loadingtime ?? "---"}</p>
-          <p>Pickup Address : {oneData.pickupaddress ?? "---"}</p>
-          <p>Start Date : {oneData.startdate ?? "---"}</p>
-          <p>Start Time :  {oneData.startime ?? "---"}</p>
-          <p> Van{oneData.vantype ?? "---"}</p>
+        <div className="bg-gray-500 rounded-2xl p-6 max-w-[1440px]  w-full m-auto  flex flex-col gap-5 text-white  text-2xl">
+          <div className="flex justify-around  ">
+            <p>Loading time</p>
+            <p>Pickup Address</p>
+            <p>Start Date</p>
+            <p>Start Time</p>
+            <p>Van </p>
+          </div>
+          {data.map((obj, index) => (
+            <div className="flex justify-around" key={index}>
+              <p className="w-[250px]"> {obj.loadingtime ?? "---"}</p>
+              <p className="w-[250px]">  {obj.pickupaddress ?? "---"}</p>
+              <p className="w-[250px]">  {obj.startdate ?? "---"}</p>
+              <p className="w-[250px]">  {obj.startime ?? "---"}</p>
+              <p className="w-[250px]">  {obj.vantype ?? "---"}</p>
+            </div>
+          ))}
         </div>
       )}
     </>
