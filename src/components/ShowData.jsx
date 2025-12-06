0c44094efa659e3b8a24fdb38c9913b9c65dead6
nikhilchauhan0105-source <nikhilchauhan0105@gmail.com>
2025-12-06 15:56:04 +0530

    data list close function done

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../utills/firebase";
 import { X } from "lucide-react";
@@ -25,6 +25,7 @@ const {setFormSteps} =useContext(informationContext)
         ...doc.data(),
       }));
       setData(dataArr);
+      console.log(dataArr)
     } catch (error) {
       alert("Something went wrong");
     } finally {
@@ -32,20 +33,31 @@ const {setFormSteps} =useContext(informationContext)
     }
   };
 
+  const handleDelete = async (id)=>{
+    const userRef =doc(db ,"User",id);
+    try{
+      await deleteDoc(userRef);
+      alert("data delete");
+      fetchData();
+
+    } catch (arr){
+      alert("something went wrong");
+    }
+  }
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  // safe first document
 
   return (
-    <section className="bg-gray-700 pt-5 h-screen">
+    <section className="bg-gray-700 pt-3 h-screen">
       {loading ? (
         <p>Loading...</p>
       ) : !data ? (
         <p>No data found</p>
       ) : (
-        <div className=" relative overflow-x-auto p-4">
+        <div className=" relative  p-4">
           <button
             onClick={() => {
               setFormSteps("/");
@@ -55,55 +67,51 @@ const {setFormSteps} =useContext(informationContext)
           >
             <X size={18} />
           </button>
-          <table className="w-full  text-left border-collapse rounded-lg overflow-hidden">
-            <thead>
-              <tr className="bg-gray-800 text-white text-sm">
-                <th className="py-3 px-4">Pickup address</th>
-                <th className="py-3 px-4">Destination address</th>
-                <th className="py-3 px-4">Van selected</th>
-                <th className="py-3 px-4">Loading time</th>
-                <th className="py-3 px-4">Start time</th>
-                <th className="py-3 px-4">Start date</th>
-                <th className="py-3 px-4 text-center">Actions</th>
-              </tr>
-            </thead>
-
-            <tbody className="bg-gray-100">
-              {data.map((obj, index) => (
-                <tr
-                  key={index}
-                  className="border-b hover:bg-gray-200 transition"
-                >
-                  <td className="py-3 px-4">{obj.pickupaddress ?? "---"}</td>
-                  <td className="py-3 px-4">
-                    {obj.destinationaddress ?? "---"}
-                  </td>
-                  <td className="py-3 px-4">
-                    {obj.vantan ?? obj.vantype ?? "---"}
-                  </td>
-                  <td className="py-3 px-4">{obj.loadingtime ?? "---"}</td>
-                  <td className="py-3 px-4">{obj.startime ?? "---"}</td>
-                  <td className="py-3 px-4">{obj.startdate ?? "---"}</td>
-
-                  <td className="py-3 px-4 flex justify-center gap-2">
-                    <button
-                      onClick={() => handleUpdate(obj, index)}
-                      className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md"
-                    >
-                      Update
-                    </button>
-
-                    <button
-                      onClick={() => handleDelete(index)}
-                      className="bg-red-600 hover:bg-red-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md"
-                    >
-                      Delete
-                    </button>
-                  </td>
+          <div className=" h-[640px]  custom-scrollbar rounded-lg  overflow-auto">
+            <table className="w-full  text-left border-collapse  ">
+              <thead>
+                <tr className="bg-gray-800 text-white text-sm">
+                  <th className="py-3 px-4">Pickup address</th>
+                  <th className="py-3 px-4">Destination address</th>
+                  <th className="py-3 px-4">Van selected</th>
+                  <th className="py-3 px-4">Loading time</th>
+                  <th className="py-3 px-4">Start time</th>
+                  <th className="py-3 px-4">Start date</th>
+                  <th className="py-3 px-4 text-center">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="bg-gray-100">
+                {data.map((obj, index) => (
+                  <tr
+                    key={index}
+                    className="border-b hover:bg-gray-200 transition"
+                  >
+                    <td className="py-3 px-4">{obj.pickupaddress ?? "---"}</td>
+                    <td className="py-3 px-4">
+                      {obj.destinationaddress ?? "---"}
+                    </td>
+                    <td className="py-3 px-4">
+                      {obj.vantan ?? obj.vantype ?? "---"}
+                    </td>
+                    <td className="py-3 px-4">{obj.loadingtime ?? "---"}</td>
+                    <td className="py-3 px-4">{obj.startime ?? "---"}</td>
+                    <td className="py-3 px-4">{obj.startdate ?? "---"}</td>
+                    <td className="py-3 px-4 flex justify-center gap-2">
+                      <button className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md">
+                        Update
+                      </button>
+                      <button
+                        onClick={() => handleDelete(obj.id)}
+                        className="bg-red-600 hover:bg-red-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </section>
