e75bd85ab9373d1d054d841b9d3ecca9e4542c94
nikhilchauhan0105-source <nikhilchauhan0105@gmail.com>
2025-12-09 11:28:23 +0530

    update fixed

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -1,22 +1,29 @@
-import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../utills/firebase";
 import { X } from "lucide-react";
 import { useContext } from "react";
 import { informationContext } from "../context/context";
-import {   useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 const ShowData = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-const {setFormSteps} =useContext(informationContext)
-  const navigate = useNavigate(); 
+  const { setFormSteps, } = useContext(informationContext);
+ const [edits, setEdits] = useState({});
 
+
+
+  const navigate = useNavigate();
 
   const fetchData = async () => {
-    setLoading
-    
-    (true);
+    setLoading(true);
     try {
       const collectionRef = collection(db, "User");
       const UserData = await getDocs(collectionRef);
@@ -25,7 +32,7 @@ const {setFormSteps} =useContext(informationContext)
         ...doc.data(),
       }));
       setData(dataArr);
-      console.log(dataArr)
+      console.log(dataArr);
     } catch (error) {
       alert("Something went wrong");
     } finally {
@@ -33,17 +40,33 @@ const {setFormSteps} =useContext(informationContext)
     }
   };
 
-  const handleDelete = async (id)=>{
-    const userRef =doc(db ,"User",id);
-    try{
+  const handleDelete = async (id) => {
+    const userRef = doc(db, "User", id);
+    try {
       await deleteDoc(userRef);
       alert("data delete");
       fetchData();
-
-    } catch (arr){
+    } catch (arr) {
       alert("something went wrong");
     }
-  }
+  };
+
+  const handleUpdate = async (id) => {
+   if(!edits[id]) {
+    alert("no changes made");
+    return
+   }
+   try {
+      const userUpdateRef = doc(db, "User", id);
+      await updateDoc(userUpdateRef, edits[id]);
+      fetchData();
+      alert("data updated");
+      setEdits((prev)=>({...prev ,[id]:{}}))
+    } catch (error) {
+      console.log(error);
+      alert("cant update data");
+    }
+  };
 
   useEffect(() => {
     fetchData();
@@ -67,7 +90,7 @@ const {setFormSteps} =useContext(informationContext)
           >
             <X size={18} />
           </button>
-          <div className=" h-[640px]  custom-scrollbar rounded-lg  overflow-auto">
+          <div className=" h-[640px]  custom-scrollbar rounded-lg  overflow-y-auto">
             <table className="w-full  text-left border-collapse  ">
               <thead>
                 <tr className="bg-gray-800 text-white text-sm">
@@ -86,18 +109,111 @@ const {setFormSteps} =useContext(informationContext)
                     key={index}
                     className="border-b hover:bg-gray-200 transition"
                   >
-                    <td className="py-3 px-4">{obj.pickupaddress ?? "---"}</td>
                     <td className="py-3 px-4">
-                      {obj.destinationaddress ?? "---"}
+                      <input
+                        type="text"
+                        value={
+                          edits[obj.id]?.pickupaddress ??
+                          obj.pickupaddress ??
+                          ""
+                        }
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              pickupaddress: e.target.value,
+                            },
+                          })
+                        }
+                      />
                     </td>
                     <td className="py-3 px-4">
-                      {obj.vantan ?? obj.vantype ?? "---"}
+                      <input
+                        type="text"
+                        value={
+                          edits[obj.id]?.destinationaddress ??
+                          obj.destinationaddress ??
+                          ""
+                        }
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              destinationaddress: e.target.value,
+                            },
+                          })
+                        }
+                      />{" "}
                     </td>
-                    <td className="py-3 px-4">{obj.loadingtime ?? "---"}</td>
-                    <td className="py-3 px-4">{obj.startime ?? "---"}</td>
-                    <td className="py-3 px-4">{obj.startdate ?? "---"}</td>
+                    <td className="py-3 px-4">
+                      <input
+                        type="text"
+                        value={edits[obj.id]?.vantype ?? obj.vantype ?? ""}
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              vantype: e.target.value,
+                            },
+                          })
+                        }
+                      />{" "}
+                    </td>
+                    <td className="py-3 px-4">
+                      <input
+                        type="text"
+                        value={
+                          edits[obj.id]?.loadingtime ?? obj.loadingtime ?? ""
+                        }
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              loadingtime: e.target.value,
+                            },
+                          })
+                        }
+                      />
+                    </td>
+                    <td className="py-3 px-4">
+                      <input
+                        type="text"
+                        value={edits[obj.id]?.startime ?? obj.startime ?? ""}
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              startime: e.target.value,
+                            },
+                          })
+                        }
+                      />{" "}
+                    </td>
+                    <td className="py-3 px-4">
+                      <input
+                        type="text"
+                        value={edits[obj.id]?.startdate ?? obj.startdate ?? ""}
+                        onChange={(e) =>
+                          setEdits({
+                            ...edits,
+                            [obj.id]: {
+                              ...edits[obj.id],
+                              startdate: e.target.value,
+                            },
+                          })
+                        }
+                      />
+                    </td>
                     <td className="py-3 px-4 flex justify-center gap-2">
-                      <button className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md">
+                      <button
+                        onClick={() => handleUpdate(obj.id)}
+                        className="bg-green-600 hover:bg-green-700 cursor-pointer text-white px-3 py-1 text-sm rounded-md"
+                      >
                         Update
                       </button>
                       <button
